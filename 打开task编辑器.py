c1649e7f7bfe6fbfd726a81ff0cfa7413300a528
DubaiJun <1533181938@qq.com>
2025-07-21 12:02:47 +0800

    更新

--- 打开task编辑器.py
+++ 打开task编辑器.py
@@ -6,8 +6,8 @@ from jsoneditonly import JsonEditor
 
 app = QApplication(sys.argv)
 keys = ['机器码', '标志', '参数', '任务名', '最晚执行时间', "操作"]
-file_name = "index.html"
-# file_name = "task.json"
+# file_name = "index.html"
+file_name = "task.json"
 
 editor = JsonEditor(keys,file_name)
 editor.show()
