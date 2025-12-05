e682cf6b420a5bb0446a776774ae3b759dcffd33
DubaiJun <1533181938@qq.com>
2025-12-05 16:47:07 +0800

    更新

---
+++ 打开pc_task编辑器.py
@@ -0,0 +1,15 @@
+import sys
+
+from PyQt5.QtWidgets import QApplication
+
+from jsoneditonly import JsonEditor
+
+app = QApplication(sys.argv)
+keys = ['机器码', '标志', '参数', '任务名', '最晚执行时间', "操作"]
+# file_name = "index.html"
+file_name = "pc_task.json"
+
+editor = JsonEditor(keys,file_name)
+editor.show()
+sys.exit(app.exec_())
+
